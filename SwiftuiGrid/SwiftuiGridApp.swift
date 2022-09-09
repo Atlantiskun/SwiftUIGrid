cc362f4c26587ad02a3944a0516e362b8cc33ebc
Dmitry <Atlantiskun@gmail.com>
2022-09-09 11:35:25 +0700

    Initial Commit

---
+++ SwiftuiGrid/SwiftuiGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiGridApp.swift
+//  SwiftuiGrid
+//
+//  Created by Дмитрий Болучевских on 09.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
